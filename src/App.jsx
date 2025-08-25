02c316de24dffef0eb4de9c73474eae23a2e74dd
Madaltso Lamusi <Astom583@gmail.com>
2025-08-25 11:02:10 +0200

    Add React frontend entry point

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Smart Chat App</h1>
+      <p>Welcome to your production-ready React frontend!</p>
+    </div>
+  );
+}
+
+export default App;
